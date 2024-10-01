363c8a969cc3be34b89f5f1ad99a3baef17cc995
vhradilenko <ext.volodymyr.hradilenko@kyriba.com>
2024-10-01 22:51:09 +0200

    for debug

--- src/main/java/config/ConfigProvider.java
+++ src/main/java/config/ConfigProvider.java
@@ -2,15 +2,20 @@ package config;
 
 import org.aeonbits.owner.ConfigFactory;
 
+import java.util.Objects;
+
 public class ConfigProvider {
     private static final String CONFIG_DIR = "src/test/resources";
     public static final ConfigProps CONFIG_PROPS;
 
     static {
         Environment env = Environment.getByCommandLineProp();
+
         switch (env) {
             case LOCAL, REMOTE ->
-                    ConfigFactory.setProperty("remote.config.path", CONFIG_DIR + "/" + env + ".properties");
+                    ConfigFactory.setProperty("remote.config.path",
+                            Objects.requireNonNull(ClassLoader.getSystemClassLoader()
+                                    .getResource(env + ".properties")).toString());
             default -> throw new RuntimeException("Provide implementation for '" + env + "' environment");
         }
         ConfigFactory.setProperty("common.config.path", CONFIG_DIR + "/common.properties");
